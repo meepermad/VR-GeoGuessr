6b1fbb070a50fdb3dcbf64d4885014b2044796b2
BubbaT05 <bjthomaszerofive@gmail.com>
2023-02-22 14:47:39 -0600

    Panoramic Switcher and 1 High Quality Photo

---
+++ Assets/Timer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeInterval = 10;
+    public float timeRemaining = 10;
+    public Texture[] Images;
+    public Material wrapper;
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wrapper.SetTexture("_MainTex", Images[0]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeRemaining > 0){
+            timeRemaining -= Time.deltaTime;
+        } else {
+            if(index == Images.Length -1){
+                wrapper.SetTexture("_MainTex", Images[0]);
+                index = 0;
+            } else{
+                index++;
+                wrapper.SetTexture("_MainTex", Images[index]);
+            }
+            timeRemaining = timeInterval;
+        }
+    }
+}
